8d42e6b5f53c77c23f7f6095d778108eb493e77d
Ebrahim khan <48891770+EIkhan@users.noreply.github.com>
2022-06-12 02:06:06 +0600

    print number

--- src/Basic/input_output/Input.java
+++ src/Basic/input_output/Input.java
@@ -2,6 +2,7 @@ package Basic.input_output;
 
 public class Input {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello java ");
+        System.out.println("7888");
     }
 }
