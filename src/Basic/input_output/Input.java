61f4e3c8b2a6373389eb1be32ee4aaa7f1794f53
Ebrahim khan <48891770+EIkhan@users.noreply.github.com>
2022-06-12 03:28:21 +0600

    primitives data types for java

--- src/Basic/input_output/Input.java
+++ src/Basic/input_output/Input.java
@@ -9,5 +9,18 @@ public class Input {
         Scanner input =new Scanner(System.in);
         System.out.println(input.nextLine()); //input string
         System.out.println(input.nextInt()); //input int number
+
+        //input a jersey number
+        System.out.println("Please input your Jersey Number: ");
+        int jerseyNumber = input.nextInt();
+        System.out.println("You Jersey Number is " + jerseyNumber);
+
+        int num =input.nextInt(); //int number
+        System.out.println(num);
+
+        String player = input.nextLine();
+        System.out.println(player);
+
+
     }
 }
