ee6f8d881318d36086c6c6de6faa4a356f607ee6
Ebrahim khan <48891770+EIkhan@users.noreply.github.com>
2022-06-12 02:22:58 +0600

    scanner input string and number

--- src/Basic/input_output/Input.java
+++ src/Basic/input_output/Input.java
@@ -1,8 +1,13 @@
 package Basic.input_output;
-
+import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
-        System.out.println("hello java ");
+        System.out.println("hello java");
         System.out.println("7888");
+
+        //scanner input string and int number
+        Scanner input =new Scanner(System.in);
+        System.out.println(input.nextLine()); //input string
+        System.out.println(input.nextInt()); //input int number
     }
 }
