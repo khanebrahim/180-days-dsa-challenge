d6d9b91b6f0a73b867e86686cc5f54788fbcf6a1
Ebrahim khan <48891770+EIkhan@users.noreply.github.com>
2022-06-13 02:17:45 +0600

    normal if/else

--- src/Basic/conditional/Condition.java
+++ src/Basic/conditional/Condition.java
@@ -1,4 +1,16 @@
 package Basic.conditional;
 
 public class Condition {
+    public static void main(String[] args) {
+        int matchWinPrice = 100000;
+
+        if (matchWinPrice>90000){
+
+            matchWinPrice += 1000;
+        }
+        else{
+            matchWinPrice += 500;
+        }
+        System.out.println(matchWinPrice);
+    }
 }
