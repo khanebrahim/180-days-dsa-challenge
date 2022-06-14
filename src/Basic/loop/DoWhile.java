284d702edc3db59679c9088940c64ade15824da2
Ebrahim khan <48891770+EIkhan@users.noreply.github.com>
2022-06-15 05:05:01 +0600

    do while loop

---
+++ src/Basic/loop/DoWhile.java
@@ -0,0 +1,11 @@
+package Basic.loop;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n =1;
+        do{
+            System.out.println("Do While");
+            n++;
+        }while(n<=5);
+    }
+}
