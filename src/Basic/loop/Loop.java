d056c84291c8adf3df4baa32722b10bc45b7fb97
Ebrahim khan <48891770+EIkhan@users.noreply.github.com>
2022-06-15 04:08:08 +0600

    loop increamnet coin

--- src/Basic/loop/Loop.java
+++ src/Basic/loop/Loop.java
@@ -1,4 +1,9 @@
 package Basic.loop;
-
+import java.util.Scanner;
 public class Loop {
+    public static void main(String[] args) {
+        for (int coin =1; coin<=5; coin++){
+            System.out.println(coin);
+        }
+    }
 }
