d36b39ab4b30cbaceb1e320053cda2420882ef9b
Ebrahim khan <48891770+EIkhan@users.noreply.github.com>
2022-06-16 07:01:20 +0600

    Fibonacci

---
+++ src/Basic/loop/Fibo.java
@@ -0,0 +1,18 @@
+package Basic.loop;
+import java.util.Scanner;
+public class Fibo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while (count <= n) {
+            int temp = b;
+            b = a+b;
+            a = temp;
+            count++;
+        }
+        System.out.println(b);
+    }
+}
