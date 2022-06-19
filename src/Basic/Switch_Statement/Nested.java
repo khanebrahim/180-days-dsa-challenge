5ef3c0a7bcf7d84ec8a8bfa507318a091aba7edf
erk75 <ebrahimkhan5222@gmail.com>
2022-06-20 02:26:55 +0600

    Nested Case better way to write

--- src/Basic/Switch_Statement/Nested.java
+++ src/Basic/Switch_Statement/Nested.java
@@ -5,31 +5,46 @@ public class Nested {
         Scanner in =new Scanner(System.in);
         int engID = in.nextInt();
         String compName = in.next();
+//        switch(engID){
+//            case 1 :
+//                System.out.println("Zulkernaine");
+//                break;
+//            case 2:
+//                System.out.println("Mukul");
+//                break;
+//            case 3:
+//                System.out.println("Kunal Kushwaha");
+//                switch(compName){
+//                    case "Google":
+//                        System.out.println("Software Engineer");
+//                        break;
+//                    case "Microsoft":
+//                        System.out.println("System Engineer");
+//                        break;
+//                    case "CIvo":
+//                        System.out.println("DevOps Engineer");
+//                        break;
+//                    default:
+//                        System.out.println("Enter a valid Company Name");
+//                }
+//                break;
+//            default:
+//                System.out.println("Enter a valid engineer ID");
+//        }
         switch(engID){
-            case 1 :
-                System.out.println("Zulkernain");
-                break;
-            case 2:
-                System.out.println("Mukul");
-                break;
-            case 3:
-                System.out.println("Kunal Kushwaha");
-                switch(compName){
-                    case "Google":
-                        System.out.println("Software Engineer");
-                        break;
-                    case "Microsoft":
-                        System.out.println("System Engineer");
-                        break;
-                    case "CIvo":
-                        System.out.println("DevOps Engineer");
-                        break;
-                    default:
-                        System.out.println("Enter a valid Company Name");
+            case 1 -> System.out.println("Zulkernaine");
+            case 2 -> System.out.println("Mukul");
+            case 3 -> {
+                System.out.println("Kunal");
+                switch (compName){
+                    case "Google" -> System.out.println("Software Engineer.");
+                    case "Microsoft" -> System.out.println("System Engineer.");
+                    case "CIvo"-> System.out.println("Devops Engineer.");
+                    default -> System.out.println("Enter a valid engID.");
                 }
-                break;
-            default:
-                System.out.println("Enter a valid engineer ID");
+            }
+            default-> System.out.println("Enter a valid compName.");
+
         }
     }
 }
