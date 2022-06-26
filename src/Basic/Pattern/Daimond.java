7fe092be0b167b7a8f8de69323983157d205c6db
erk75 <ebrahimkhan5222@gmail.com>
2022-06-27 00:55:01 +0600

    Daimond pattern

---
+++ src/Basic/Pattern/Daimond.java
@@ -0,0 +1,24 @@
+package Basic.Pattern;
+
+public class Daimond {
+    public static void main(String[] args) {
+        int n =5;
+        pattern5(n);
+    }
+    static void pattern5(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalColRow = row>n ? 2*n -row:row;
+            int noOfSpaces = n- totalColRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+
+            }
+            for (int col = 0; col < totalColRow; col++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+    }
+}
+
