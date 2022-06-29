b3b8e26e163623b2b7e872b9b9866b569b7a62e3
erk75 <ebrahimkhan5222@gmail.com>
2022-06-29 23:24:36 +0600

    NumberPyramid

---
+++ src/Basic/Pattern/NumberPyramid.java
@@ -0,0 +1,23 @@
+package Basic.Pattern;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int n =5;
+        pattern6(n);
+    }
+    static void pattern6(int n){
+        for (int row = 1; row <=n; row++) {
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col>=1 ; col--) {
+                System.out.print(col + " ");
+
+            }
+            for (int col=2; col<=row ; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
