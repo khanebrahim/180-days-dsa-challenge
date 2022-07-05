3cc279e54535766d688ba64175994a16a5a1a7eb
erk75 <ebrahimkhan5222@gmail.com>
2022-07-05 12:49:55 +0600

    pattern another

--- src/Basic/Pattern/NumberPyramid.java
+++ src/Basic/Pattern/NumberPyramid.java
@@ -21,6 +21,6 @@ public class NumberPyramid {
 
 
             System.out.println();
-        }
+        } 
     }
 }
---
+++ src/Basic/Pattern/PatternNum.java
@@ -0,0 +1,28 @@
+package Basic.Pattern;
+
+public class PatternNum {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern17(n);
+    }
+
+    static void pattern17(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+
+            int c = row > n ? 2 * n - row : row;
+
+            for (int space = 0; space < n - c; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
