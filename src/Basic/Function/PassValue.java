04b15fe8126f81611767d47955cab80391cd1be8
erk75 <ebrahimkhan5222@gmail.com>
2022-07-09 12:30:54 +0600

    String example

--- src/Basic/Function/PassValue.java
+++ src/Basic/Function/PassValue.java
@@ -1,6 +1,6 @@
 package Basic.Function;
 import java.util.Scanner;
-public class PassValue {
+public class  PassValue {
     public static void main(String[] args) {
        int ans= sum(20,45);
         System.out.println(ans);
--- src/Basic/Function/StringF.java
+++ src/Basic/Function/StringF.java
@@ -2,9 +2,17 @@ package Basic.Function;
 
 public class StringF {
     public static void main(String[] args) {
-        String message = greet();
-        System.out.println(message);
+//        String message = greet();
+//        System.out.println(message);
+        String personalInfo = myInfo("Safa");
+        System.out.println(personalInfo);
     }
+
+     static String myInfo(String name) {
+        String message = "Hi " + name;
+        return  message;
+    }
+
     static String greet(){
         String greeting = "How are you";
         return greeting;
