e98826e4674abe8b0d62e59053b9b7f8b47e9e9d
erk75 <ebrahimkhan5222@gmail.com>
2022-07-09 11:47:10 +0600

    String

---
+++ src/Basic/Function/StringF.java
@@ -0,0 +1,12 @@
+package Basic.Function;
+
+public class StringF {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        String greeting = "How are you";
+        return greeting;
+    }
+}
