369fd06c01e12a7853f9ebf21208e44d8d576539
erk75 <ebrahimkhan5222@gmail.com>
2022-07-09 12:17:49 +0600

    Pass parameter

---
+++ src/Basic/Function/PassValue.java
@@ -0,0 +1,12 @@
+package Basic.Function;
+import java.util.Scanner;
+public class PassValue {
+    public static void main(String[] args) {
+       int ans= sum(20,45);
+        System.out.println(ans);
+    }
+    static int  sum( int a,int b){
+        int result =a+b;
+        return result;
+    }
+}
