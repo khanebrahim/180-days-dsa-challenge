da775e0a50cdb32893e6feda6dfdec0eba169d40
erk75 <ebrahimkhan5222@gmail.com>
2022-07-11 21:59:51 +0600

    swap

---
+++ src/Basic/Function/Swap.java
@@ -0,0 +1,17 @@
+package Basic.Function;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a=20,b=40,temp;
+//        temp =a;
+//        a=b;
+//        b=temp;
+        swap(a,b);
+        System.out.println(a+" "+b);
+    }
+    static void swap(int a,int b){
+        int temp =a;
+        a=b;
+        b=temp;
+    }
+}
