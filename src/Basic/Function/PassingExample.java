9751b414fa95c4afb40c6617d5126965cc67fd9e
erk75 <ebrahimkhan5222@gmail.com>
2022-07-14 15:11:28 +0600

    pass value example

---
+++ src/Basic/Function/PassingExample.java
@@ -0,0 +1,11 @@
+package Basic.Function;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Ebrahim khan";
+        greet(name);
+    }
+    static void greet(String naam){
+        System.out.println(naam);
+    }
+}
