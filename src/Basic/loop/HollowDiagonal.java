be4abec97cd787679aed19a0691c6bf22949d50e
erk75 <ebrahimkhan5222@gmail.com>
2022-07-20 01:33:29 +0600

    Hollowdiagonal

---
+++ src/Basic/loop/HollowDiagonal.java
@@ -0,0 +1,25 @@
+package Basic.loop;
+import java.util.Scanner;
+public class HollowDiagonal {
+    public static void main(String[] args) {
+        int i,j,rows,cols;
+        System.out.println("Hollow Square:");
+        Scanner in = new Scanner(System.in);
+        System.out.println("rows:");
+        rows=in.nextInt();
+        System.out.println("Cols:");
+        cols=in.nextInt();
+        for (i = 1; i <=rows; i++) {
+            for (j = 1; j<=cols; j++) {
+                if(i==1 || i==rows || j==1 || j==cols || i==j || j==(cols-i+1)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
