4ef12f8ec35fa0111c9dfc51aff268f0c4c4d92f
erk75 <ebrahimkhan5222@gmail.com>
2022-07-20 14:11:29 +0600

    MirroredRhombus

---
+++ src/Basic/loop/MirroredRhombus.java
@@ -0,0 +1,21 @@
+package Basic.loop;
+import java.util.Scanner;
+public class MirroredRhombus {
+    public static void main(String[] args) {
+        int i,j,n;
+        System.out.println("Input Number: ");
+        Scanner in = new Scanner(System.in);
+        n= in.nextInt();
+        for (i = 1; i<=n ; i++) {
+            for (j = 1; j<=n+i ; j++) {
+                System.out.print(" ");
+
+            }
+            for ( j = 1; j<=n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
