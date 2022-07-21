6b4d5702a7ed214ef5d0d491764c5e42b3ad0b86
erk75 <ebrahimkhan5222@gmail.com>
2022-07-22 03:07:11 +0600

    HollowRightTraingle

---
+++ src/Basic/loop/HollowRightTraingle.java
@@ -0,0 +1,22 @@
+package Basic.loop;
+import java.util.Scanner;
+public class HollowRightTraingle {
+    public static void main(String[] args) {
+        int i, j, n;
+        System.out.println("Input user Number :");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
