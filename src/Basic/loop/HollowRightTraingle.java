296c94795d1103f6a11e47c47ac680d2060db40f
erk75 <ebrahimkhan5222@gmail.com>
2022-07-23 22:54:39 +0600

    Hollow Right Traingle

--- src/Basic/loop/HollowRightTraingle.java
+++ src/Basic/loop/HollowRightTraingle.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HollowRightTraingle {
     public static void main(String[] args) {
         int i, j, n;
-        System.out.println("Input user Number :");
+        System.out.println("Input user Number:");
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
         for (i = 1; i <= n; i++) {
