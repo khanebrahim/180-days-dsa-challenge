c9777a65135d6f2c210bad8a1151a3762353d687
erk75 <ebrahimkhan5222@gmail.com>
2022-07-25 23:25:22 +0600

    Index Number

---
+++ src/Array/IndexNumber.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class IndexNumber {
+    public static void main(String[] args) {
+        int [] arr = {89,45,86,23,27};
+        for(int  i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
