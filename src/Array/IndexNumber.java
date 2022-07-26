5a9a412f2d39288b931b2e4ced768db4c1447bc6
erk75 <ebrahimkhan5222@gmail.com>
2022-07-26 12:16:14 +0600

    Index

--- src/Array/IndexNumber.java
+++ src/Array/IndexNumber.java
@@ -3,8 +3,9 @@ package Array;
 public class IndexNumber {
     public static void main(String[] args) {
         int [] arr = {89,45,86,23,27};
+        String [] names = {"Kalim", "jalim","shimul","salim","maruf"};
         for(int  i = 0; i<arr.length; i++){
-            System.out.println(arr[i]);
+            System.out.println(names[i] + "->" + arr[i]);
         }
     }
 }
