383f8707022e856f20382d41f1c3030c19ac2a70
erk75 <ebrahimkhan5222@gmail.com>
2022-08-08 11:39:18 +0600

    celciusToFurenhiet Math

--- src/Basic/Function/Tempareture.java
+++ src/Basic/Function/Tempareture.java
@@ -1,9 +1,21 @@
 package Basic.Function;
-
+import java.util.Scanner;
 public class Tempareture {
     public static void main(String[] args) {
-        System.out.println(celciusToFurenhiet(30));
-        System.out.println(furenhietToCelcius(86));
+        Scanner sc = new Scanner(System.in);
+        double temp;
+        String type;
+        System.out.println("Enter Temperature (to convert enter C or F)");
+        temp = sc.nextDouble();
+        type = sc.next();
+        if (type.equals("C")){
+            System.out.println("Furenhiet = " + temp+ ";Celcius=" + furenhietToCelcius(temp));
+        } else if (type.equals("F")) {
+            System.out.println("Furenhiet = " + celciusToFurenhiet(temp) +"; Celcius = " + temp);
+        }
+        else {
+            System.out.println(" input either C or F");
+        }
 
     }
     public static double celciusToFurenhiet(double celcius){
