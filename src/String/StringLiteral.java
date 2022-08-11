0e06d52864a1966125cd3e52aba97515abaac7cf
erk75 <ebrahimkhan5222@gmail.com>
2022-08-11 23:51:50 +0600

    String

---
+++ src/String/StringLiteral.java
@@ -0,0 +1,9 @@
+package String;
+
+public class StringLiteral {
+    public static void main(String[] args) {
+        byte[] bt = {65,66,67};
+        String str = new String(bt);
+        System.out.println(str);
+   }
+}
