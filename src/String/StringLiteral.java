578769b6f503ae767d89172562154f142106537d
erk75 <ebrahimkhan5222@gmail.com>
2022-08-14 13:50:34 +0600

    String literal

--- src/String/StringLiteral.java
+++ src/String/StringLiteral.java
@@ -1,9 +1,11 @@
 package String;
-
 public class StringLiteral {
     public static void main(String[] args) {
-        byte[] bt = {65,66,67};
-        String str = new String(bt);
-        System.out.println(str);
+//        byte[] bt = {65,66,67};
+//        String str = new String(bt);
+//        System.out.println(str);
+          String str = "Java is a famous language";
+          String str3 = "Java is a famous language";
+        System.out.println(str==str3);
    }
 }
