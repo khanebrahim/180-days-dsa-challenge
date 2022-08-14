794b1f9b22f8d68e3f6ae3d7362791566b0f3929
erk75 <ebrahimkhan5222@gmail.com>
2022-08-14 14:26:23 +0600

    String literal and constructor

--- src/String/StringLiteral.java
+++ src/String/StringLiteral.java
@@ -3,9 +3,20 @@ public class StringLiteral {
     public static void main(String[] args) {
 //        byte[] bt = {65,66,67};
 //        String str = new String(bt);
-//        System.out.println(str);
-          String str = "Java is a famous language";
-          String str3 = "Java is a famous language";
-        System.out.println(str==str3);
+//        System.out.println(str); // A,B,C
+
+//          String str = "Java is a famous language";
+//          String str3 = "Java is a famous language";
+//          System.out.println(str==str3); //true
+
+            String str3 = new String("Java is my fav language");
+            String str4 = new String("Java is my fav language");
+            String str5 = new String("java is my fav language");
+//          System.out.println(str3==str4); //false
+
+          // System.out.println(str3.equals(str4));  //true
+
+        System.out.println(str3.equalsIgnoreCase(str5)); //true
+
    }
 }
