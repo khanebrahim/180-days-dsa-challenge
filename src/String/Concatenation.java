786c2408a6b03a15b402b974219dca557d7b3255
erk75 <ebrahimkhan5222@gmail.com>
2022-08-15 17:49:23 +0600

    String concatenation

---
+++ src/String/Concatenation.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String FirstName = "Ebrahim";
+        String LastName = "Iqbal Khan";
+        String FullName = FirstName + " " + LastName;
+        System.out.println(FullName);
+        int age = 22;
+        String str = age + "";
+        System.out.println(str);
+
+    }
+}
