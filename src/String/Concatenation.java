b65368dba84cb7aea25ea0ce4906d17bffa28c2f
erk75 <ebrahimkhan5222@gmail.com>
2022-08-16 23:55:10 +0600

    String concatenation and method

--- src/String/Concatenation.java
+++ src/String/Concatenation.java
@@ -2,13 +2,25 @@ package String;
 
 public class Concatenation {
     public static void main(String[] args) {
-        String FirstName = "Ebrahim";
-        String LastName = "Iqbal Khan";
-        String FullName = FirstName + " " + LastName;
-        System.out.println(FullName);
+        String firstName = "Ebrahim Khan";
+        String lastName = "Iqbal Khan";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
         int age = 22;
         String str = age + "";
         System.out.println(str);
+        for (int i = 0; i <firstName.length(); i++) {
+            char ch = firstName.charAt(i);
+            if(ch=='a'){
+                System.out.print(ch + " ");
+            }
+        }
+        System.out.println(firstName.compareTo(lastName));
+        System.out.println(lastName.compareTo(firstName));
+        String finalName = firstName.concat(lastName);
+        System.out.println(lastName.contains("Eb"));
+        System.out.println(finalName);
+
 
     }
 }
