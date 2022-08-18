dbfdfe82f27029bdaebc9a0c2056633c775b4265
erk75 <ebrahimkhan5222@gmail.com>
2022-08-18 16:40:51 +0600

    String Concatenationan and Method

--- src/String/Concatenation.java
+++ src/String/Concatenation.java
@@ -15,11 +15,22 @@ public class Concatenation {
                 System.out.print(ch + " ");
             }
         }
-        System.out.println(firstName.compareTo(lastName));
-        System.out.println(lastName.compareTo(firstName));
-        String finalName = firstName.concat(lastName);
-        System.out.println(lastName.contains("Eb"));
-        System.out.println(finalName);
+//        System.out.println(firstName.compareTo(lastName));
+//        System.out.println(lastName.compareTo(firstName));
+//        String finalName = firstName.concat(lastName);
+//        System.out.println(lastName.contains("Kh"));
+//        System.out.println(finalName);
+//        System.out.println(firstName.startsWith("Eb"));
+//        System.out.println(firstName.endsWith("an"));
+//        System.out.println(firstName.indexOf('a'));
+
+        String middleName = firstName.substring(6);
+        System.out.println(middleName);
+
+        char[] names = firstName.toCharArray();
+        for (int i = 0; i< names.length;i++){
+            System.out.println(names[i]);
+        }
 
 
     }
