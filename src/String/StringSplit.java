6f12277fa7787b4e5eba7fa21611e312bc47025b
erk75 <ebrahimkhan5222@gmail.com>
2022-08-19 16:11:24 +0600

    String Split and tokenizer

---
+++ src/String/StringSplit.java
@@ -0,0 +1,20 @@
+package String;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class StringSplit {
+    public static void main(String[] args) {
+        String players = "Shakib Devilliers Rohit Babar Buttler Warner Moen Hardik Rashiid Shahin Bumrah";
+
+          String[] player = players.split(" ");
+//        System.out.println(Arrays.toString(player));
+//        System.out.println(player[2]);
+        StringTokenizer token = new StringTokenizer(players);
+        while(token.hasMoreTokens()){
+            System.out.println(token.countTokens());
+            System.out.println(token.nextToken().toUpperCase());
+        }
+
+    }
+}
