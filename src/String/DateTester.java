6491102524b8add0fa00997056a1bc66fd99d824
erk75 <ebrahimkhan5222@gmail.com>
2022-08-23 23:04:34 +0600

    date checking

---
+++ src/String/DateTester.java
@@ -0,0 +1,9 @@
+package String;
+
+public class DateTester {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        Date date = new Date(1661273569742L);
+        System.out.println(date);
+    }
+}
