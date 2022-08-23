cbb2138444a65e3d46ae69136ebdb0acdb700f84
erk75 <ebrahimkhan5222@gmail.com>
2022-08-23 23:18:16 +0600

    date check

--- src/String/DateTester.java
+++ src/String/DateTester.java
@@ -1,9 +1,11 @@
 package String;
 
+import java.util.Date;
+
 public class DateTester {
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
-        Date date = new Date(1661273569742L);
+        Date date = new Date(1661275043044L);
         System.out.println(date);
     }
 }
