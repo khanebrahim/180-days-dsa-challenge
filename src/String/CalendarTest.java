c8aa32c0af09714bba1306ec4e277dafbd3e219e
erk75 <ebrahimkhan5222@gmail.com>
2022-08-29 21:10:55 +0600

    Calenda api

---
+++ src/String/CalendarTest.java
@@ -0,0 +1,14 @@
+package String;
+import java.util.Calendar;
+public class CalendarTest {
+    public static void main(String[] args) {
+        Calendar now = Calendar.getInstance();
+//        System.out.println(now);
+        System.out.println("hour : " + now.get(Calendar.HOUR));
+        System.out.println("minute : " + now.get(Calendar.MINUTE));
+        System.out.println("month : " + now.get(Calendar.MONTH));
+        System.out.println("month : " + now.get(Calendar.DAY_OF_MONTH));
+        System.out.println("year : " + now.get(Calendar.YEAR));
+        System.out.println("month : " + now.get(Calendar.WEEK_OF_MONTH));
+    }
+}
--- src/String/SimpleDateFormatTest.java
+++ src/String/SimpleDateFormatTest.java
@@ -5,9 +5,9 @@ import java.util.Date;
 public class SimpleDateFormatTest {
     public static void main(String[] args) throws ParseException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
-//        String dateString = "2022-8-27";
-//        Date date = simpleDateFormat.parse(dateString);
-//        System.out.println(date);
+//      String dateString = "2022-8-27";
+//      Date date = simpleDateFormat.parse(dateString);
+//      System.out.println(date);
         Date date = new Date();
         System.out.println(simpleDateFormat.format(date));
 
