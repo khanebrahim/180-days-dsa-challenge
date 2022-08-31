dbb8f69efd85c42d273c10418518f794a1817191
erk75 <ebrahimkhan5222@gmail.com>
2022-08-31 22:45:49 +0600

    Gregorian Calendar api

--- src/String/GregorianCalendarTest.java
+++ src/String/GregorianCalendarTest.java
@@ -3,9 +3,12 @@ import java.util.GregorianCalendar;
 public class GregorianCalendarTest {
     public static void main(String[] args) {
         GregorianCalendar calendar = new GregorianCalendar();
+
         GregorianCalendar calendar1 = new GregorianCalendar(2022, 10, 1,0,0,25);
+        GregorianCalendar calendar2 = new GregorianCalendar(2023, 5, 23,0,0,25);
         System.out.println(calendar.getTime());
         System.out.println(calendar1.getTime());
+        System.out.println(calendar2);
     }
 }
 
